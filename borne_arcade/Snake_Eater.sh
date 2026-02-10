4a37bf5cde7b0dea7bb6c923121072d60a0aeb00
TarzanHR <leny621000@gmail.com>
2026-02-10 09:31:31 +0100

    fix: fixing jar path for MG2D

--- borne_arcade/Snake_Eater.sh
+++ borne_arcade/Snake_Eater.sh
@@ -4,4 +4,4 @@ source "$(dirname "$0")/common.sh"
 xdotool mousemove 1280 1024
 cd "$BORNE_ROOT/projet/Snake_Eater"
 touch highscore
-java -cp ".:../..:$(dirname "$MG2D_PATH")" Snake_Eater
+java -cp ".:../..:$MG2D_JAR" Snake_Eater
